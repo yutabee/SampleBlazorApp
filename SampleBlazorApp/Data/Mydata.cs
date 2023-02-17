4aaf0850c7d49271d50a3449da10fe1f00d4f0b5
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-17 15:44:52 +0900

    [add] #5 Mydata.cs,/Sample.razor/SampleList.razor/NavMenu.razorを作成 コンポーネントテンプレートを作成しました。

---
+++ SampleBlazorApp/Data/Mydata.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleBlazorApp.Data;
+
+public class Mydata
+{
+    [Required]
+    public string Name { get; set; }
+    public string Password { get; set; }
+    [EmailAddress]
+
+    public string Mail { get; set; }
+
+    public Mydata(string Name, string Password, string Mail)
+    {
+        this.Name = Name ?? "my name.";
+        this.Password = Password ?? "";
+        this.Mail = Mail ?? "";
+    }
+
+    public override string ToString()
+    {
+        return "[" + Name + " (" + Password + ") " + Mail + "]";
+    }
+}
