d896234a290a65a80c1fec4b46c31cbfc55d416e
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-17 17:24:46 +0900

    [add] #6 サービスプログラムを作成 MydataService.csを作成し、Sample.razorで利用するプログラムを作成

---
+++ SampleBlazorApp/Data/MydataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace SampleBlazorApp.Data;
+
+public class MydataService
+{
+    private List<Mydata> datos = new()
+    {
+        new Mydata("Taro","hoge","taro@yamada"),
+        new Mydata("Hanako","foo","hanako@flower"),
+        new Mydata("Sachiko","bar","sachiko@happy")
+    };
+
+    public Task<Mydata> GetMydataAsync(int n)
+    {
+        int num = n < 0 ? 0 : n > datos.Count ? datos.Count : n;
+        return Task.FromResult(datos[n]);
+    }
+}
