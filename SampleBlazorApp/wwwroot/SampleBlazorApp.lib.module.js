906f7731220994acfe7a4055933e45af1368934f
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 11:06:28 +0900

    [add] #8 Created SampleBlazorApp.lib.module.js Javascriptのライブラリをモジュールとして実装

---
+++ SampleBlazorApp/wwwroot/SampleBlazorApp.lib.module.js
@@ -0,0 +1,17 @@
+﻿//Blazorを開始する前に呼び出される関数
+export function beforeStart(options, extensions) {
+    console.log("before Start");
+}
+//Blazorの準備が完了したら呼び出される関数
+export function afterStarted(blazor) {
+    console.log("after Started");
+}
+//独自に作成した処理
+//windowプロパティにメソッドとして関数を代入
+window.say = (message) => {
+    alert(message);
+}
+
+window.ask = (message) => {
+    return prompt(message, '');
+}
