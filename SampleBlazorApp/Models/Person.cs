fee799ce8f212e805155e7225e83b4d8f4efeb28
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-19 22:44:53 +0900

    [add] #9 Personモデルの作成

---
+++ SampleBlazorApp/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleBlazorApp.Models;
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+    public string? Mail { get; set; }
+    public int Age { get; set; }
+}
